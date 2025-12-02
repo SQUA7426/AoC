7dfc37b677fcbeb6ce456b09d796b064979440ae
Timothy <timo-drexler@online.de>
2025-12-02 04:22:53 +0100

    shortened day1.scala

--- 2025/day1/day1.scala
+++ 2025/day1/day1.scala
@@ -4,17 +4,27 @@ object Solution {
   def main(args: Array[String]): Unit = {
     // val file = "./example.txt"
     val file = "./input.txt"
-    dial(file, 50)
-    println()
+    println(s"onto: ${dial(file, 50)}")
   }
+
   def dial(file: String, input: Int): Int = {
-    var left = 0
+    var ontoZero = 0
     var sum: Int = input
     val s = fromFile(file).getLines().toSeq.map(t => t.splitAt(1)).collect {
-      case (rotation, distance) => { var temp = distance.toInt; (if (temp > 99) { while (temp>99) { temp -= 100}}); (rotation,temp)}
+      case (rotation, distance) => {
+        var temp = distance.toInt;
+        (if (temp > 99) { while (temp > 99) { temp -= 100 } }); (rotation, temp)
+      }
     }
-    val r = s.map { case(rotation,distance) => /*println(sum);*/ if (rotation=="R") {if (sum + distance.toInt > 99) sum += (distance.toInt - 100) else {sum += distance.toInt}}; if (rotation=="L"){if (sum - distance.toInt < 0) { sum += (100 - distance.toInt)} else sum -= distance.toInt }; if (sum == 0) left+=1 }
-    print(f"left ${left}") // 1074
-    left
+    val r = s.map {
+      case (rotation, distance) => { /*println(sum);*/ if (rotation == "R") {
+          sum += (distance.toInt - (if (sum + distance.toInt > 99) { 100 } else 0))
+        } else {
+          sum -= (distance.toInt - (if (sum - distance.toInt < 0) { 100 } else 0))
+        }
+        if (sum == 0) ontoZero += 1
+      }
+    }
+    ontoZero
   }
 }
